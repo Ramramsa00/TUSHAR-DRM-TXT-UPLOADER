462141b16f3eed050a09a154c179cffd261ee326
ramramsa <tanushree8177@gmail.com>
2026-02-02 03:31:01 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -379,7 +379,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     emoji = get_next_emoji()
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:02 -vframes 1 "{filename}.jpg"', shell=True)   
     await prog.delete (True)   
-    reply = await m.reply_text(f"🚀🚀𝗨𝗣𝗟𝗢𝗔𝗗𝗜𝗡𝗚🚀🚀🚀** » `{name}`\n\n🤖𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 ➤MANISH")   
+    reply = await m.reply_text(f"🚀🚀𝗨𝗣𝗟𝗢𝗔𝗗𝗜𝗡𝗚🚀🚀🚀** » `{name}`\n\n🤖𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 ➤❣𓊈Ⅾ𝔯Ṃũ𝕤īcī𝗮𝚗𓊉❣(✿◠‿◠)")   
     try:   
         if thumb == "no":   
             thumbnail = f"{filename}.jpg"   
